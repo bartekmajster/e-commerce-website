ceff9a4162cbf9453013686c89d32fe69376e50c
Bartłomiej Kochanowicz <bartlomiej.kochanowicz@gmail.com>
2020-02-24 13:19:53 +0100

    new conception of pages

--- src/scripts/shop-page.js
+++ src/scripts/shop-page.js
@@ -1,1 +1,14 @@
 import 'styles/pages/shop-page.scss';
+
+const informations = [...document.querySelectorAll('.product__info')];
+
+informations.forEach((info) => {
+	info.addEventListener('mouseover', () => {
+		// eslint-disable-next-line no-param-reassign
+		info.style.opacity = '1';
+	});
+	info.addEventListener('mouseout', () => {
+		// eslint-disable-next-line no-param-reassign
+		info.style.opacity = '0';
+	});
+});
